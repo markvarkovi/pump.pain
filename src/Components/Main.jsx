6926bbb2b01534b2633bf5ba5295e1b6582285d8
Mark Varkovi <vmark33@gmail.com>
2024-04-10 10:45:40 +0200

    added harold faceswap

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,23 @@
 /* eslint-disable react/no-unescaped-entities */
 import Header from "./Header.jsx";
+import { useState } from "react";
 
 export default function Main() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    "https://i.ibb.co/ZKzWcwG/Harold-1.png",
+    "https://i.ibb.co/qrv2FJF/Harold-2.png",
+    "https://i.ibb.co/stJsHMP/Harold-3.png",
+    "https://i.ibb.co/yVQdPVt/Harold-4.png",
+    "https://i.ibb.co/k14tb4p/Harold-5.png",
+    "https://i.ibb.co/x3k7c6y/Harold-6.png",
+    "https://i.ibb.co/7W9pq20/Harold-7.png",
+  ];
+
+  const handleReleaseAnger = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
   return (
     <>
       <div className="flex w-full h-screen flex-col justify-between">
@@ -12,18 +28,17 @@ export default function Main() {
               Welcome to the pump.pain community!
             </h1>
             <p>
-              A community where we understand the agony and
-              frustration that comes with investing in meme
-              coins only to be rug-pulled. We know the pain
-              all too well. The sinking feeling in your
-              stomach, the disbelief as your investment
+              A community where we understand the agony and frustration that comes with
+              investing in meme coins only to be rug-pulled. We know the pain all too
+              well. The sinking feeling in your stomach, the disbelief as your investment
               evaporates into thin air.
             </p>
             <br></br>
-            <p className="mb-4">
-              But here, you're NOT ALONE.
-            </p>
-            <button className="mt-8 bg-gradient-to-t from-red-400 to-gray border border-solid border-black hover:from-red-500 hover:to-red text-white font-bold p-6 rounded-full focus:outline-none focus:shadow-outline transition duration-300 ease-in-out">
+            <p className="mb-4">But here, you're NOT ALONE.</p>
+            <button
+              className="mt-8 bg-gradient-to-t from-red-400 to-gray border border-solid border-black hover:from-red-500 hover:to-red 
+            text-white font-bold p-6 rounded-full focus:outline-none focus:shadow-outline transition duration-300 ease-in-out"
+            >
               Learn more about our mission
             </button>
           </div>
@@ -32,11 +47,15 @@ export default function Main() {
             style={{ height: "30rem", width: "150rem" }}
           >
             <img
-              src="https://i.ibb.co/ZKzWcwG/Harold-1.png"
-              alt="Harold-1"
+              src={images[currentImageIndex]}
+              alt={`Image ${currentImageIndex + 1}`}
               className="p-4 object-contain w-full h-full cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110"
             />
-            <button className="text-2xl mt-8 bg-gradient-to-t from-red-400 to-gray border border-solid border-black hover:from-red-500 hover:to-red text-white font-bold p-6 rounded-full focus:outline-none focus:shadow-outline transition duration-300 ease-in-out">
+            <button
+              className="text-2xl mt-8 bg-gradient-to-t from-red-400 to-gray border border-solid border-black hover:from-red-500 hover:to-red
+             text-white font-bold p-6 rounded-full focus:outline-none focus:shadow-outline transition duration-300 ease-in-out"
+              onClick={handleReleaseAnger}
+            >
               Release Anger
             </button>
           </div>
