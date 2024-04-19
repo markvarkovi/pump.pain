3a6d265cff77fb1a648dd888263de415a7634e5d
Mark Varkovi <vmark33@gmail.com>
2024-04-19 21:26:28 +0200

    added hamlana

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,13 +8,13 @@ export default function Home() {
   }
 
   const images = [
-    "https://pbs.twimg.com/media/GIAeBtqbAAEn_KB.jpg",
-    "https://i.ibb.co/gFQV4vw/Qm-Q1ngjp1ja-PJn91t-S36-BWopn5-UGtx1srh-Fgzcriv5wd-Ks.png",
-    "https://i.ibb.co/Vjrb349/Qme-Kz-Hi7g-XTCKn-Ywznf1-JYm-KXT1t-Z1gpkeq-MSWJ5p-JGKxj.png",
-    "https://i.ibb.co/MSQ87zQ/Qmcf-RSi7-M7f-Aj7hr2-WU6ju-J7-JDg3-JZ6w-BZy-A3-Arr17boa-L.png",
-    "https://i.ibb.co/cL3gQLb/Qmf2-MJ82-TE9rc7-CGLMNe-SA4-Kysj-V7q6o-Np-Wfj-L2-Vpi-VNAA.jpg",
-    "https://i.ibb.co/KW5mvgy/boden.webp",
-    "https://i.ibb.co/4KzDMhP/jeo-boden-meme-g-ID-7.webp",
+    "https://i.ibb.co/6FZJjM5/586c3354-e43d-4595-b9ca-0123b663111a.webp",
+    "https://i.ibb.co/nQqMtPp/be2944bb-d650-462f-a171-6da3030e1af1.webp",
+    "https://i.ibb.co/GtTfgVc/08ba31b8-4018-459b-b524-e552ea13edcd.webp",
+    "https://i.ibb.co/9HfFHLx/12fe87e8-37b6-402b-94d8-2278475c8d51.webp",
+    "https://i.ibb.co/6cSVxwF/ddacce69-d8bd-4eb6-a5cf-4666a3c2cec9.webp",
+    "https://i.ibb.co/mC328mb/d0c07b58-647c-44b9-b89d-fa6ec2225606.webp",
+    "https://i.ibb.co/72Vb6yp/5588aabe-06cf-4eea-96fc-a330c18f9c47.webp",
   ];
 
   const getHimMad = () => {
@@ -41,9 +41,7 @@ export default function Home() {
     <>
       <div className="flex items-start my-28">
         <div className="mx-64 text-gray-50 tracking-widest text-lg ">
-          <h1 className="my-16 font-bold text-2xl">
-            Welcome to the BODENCERS community!
-          </h1>
+          <h1 className="my-16 font-bold text-2xl">Welcome to the HamLana community!</h1>
           <p>
             A community where we understand the agony and frustration that comes with
             investing in meme coins only to be rug-pulled. We know the pain all too well.
@@ -52,7 +50,7 @@ export default function Home() {
           </p>
           <p>But here, you are NOT ALONE.</p>
           <br />
-          <p className="mb-4 text-2xl">The solution is us. The solution is $BODENCE.</p>
+          <p className="mb-4 text-2xl">The solution is us. The solution is $HAMLANA.</p>
           <button
             className="mt-8 bg-gradient-to-t from-blue-700 to-gray border border-solid border-yellow-800 border-2 hover:from-red-500 hover:to-red 
       text-white font-bold p-6 rounded-full focus:outline-none focus:shadow-outline transition duration-300 ease-in-out text-4xl"
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,14 @@ export default function Navbar() {
   return (
     <>
       <div className="bg-yellow-300 bg-opacity-90 text-black text-3xl bold flex justify-center font-bold tracking-widest overflow-hidden py-4">
-        <p>🚀 LISTING $BODENCE IS LIVE ON PUMP.FUN 🚀 &nbsp;</p>
+        <p>🚀 LISTING $HAMLANA IS LIVE ON PUMP.FUN 🚀 &nbsp;</p>
       </div>
       <header className="text-gray-50 mb-0 px-4 w-full flex items-center justify-center">
         <div className="flex items-center space-x-10 2xl:space-x-40 tracking-widest text-sm 2xl:text-2xl cursor-pointer">
           <div id="pictures" className="flex mt-8">
             <img
-              src="https://i.ibb.co/c37Pf8Y/bodance.png"
-              className="w-40 h-40 rounded-full border border-4 border-gray-50"
+              src="https://i.ibb.co/vsj4B9Q/super.webp"
+              className="w-48 h-48 rounded-full"
               alt="logo"
               onClick={() => window.location.reload()}
             />
@@ -23,13 +23,13 @@ export default function Navbar() {
             <Link to="/about">ABOUT</Link>
           </div>
           <div>
-            <Link to="/ruglist">LIST OF BODENCERS</Link>
+            <Link to="/ruglist">LIST OF HAMLANAS</Link>
           </div>
           <div>
             <Link to="/social">SOCIALS</Link>
           </div>
           <div>
-            <Link to="/join">JOIN BODENCE</Link>
+            <Link to="/join">JOIN HAMLANAS</Link>
           </div>
         </div>
       </header>
