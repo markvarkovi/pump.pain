bed6852e301441c2bd979604ad774428a8540cab
Varkovi Mark <vmark33@gmail.com>
2024-04-25 03:09:32 +0200

    added links

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,7 +1,7 @@
 export default function Socials() {
   const tg = "https://t.me/+QiRqhErmM0A2MjM0";
   const x = "https://twitter.com/PumpPain53294";
-  const pump = ""; //need to be added when listing is done
+  const pump = "https://pump.fun/CsETM4yQUjV76egCaSZBn6yxp9W8BtLh6Adrp9hYKAfk";
   const tiktok = "https://www.tiktok.com/@pump.pain";
 
   function redirectTo(link) {
