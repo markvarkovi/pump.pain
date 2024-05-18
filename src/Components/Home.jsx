32c421b2f73662dcfe268cb59fc777fa0652e6e3
Mark Varkovi <vmark33@gmail.com>
2024-05-19 01:34:02 +0200

    fixed tg link

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   function telegramRedirect() {
-    window.location.href = "https://t.me/+tjzoRJK5fRI0ZDlk";
+    window.location.href = "https://t.me/+82EDc5HK7Pc4MmJk";
   }
 
   const images = [
